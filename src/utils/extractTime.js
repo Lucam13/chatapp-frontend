bbe679ffea7878adc8c11e0e0b72c1043f0e6662
Lucam13 <lucc_88@hotmail.com>
2024-08-26 14:20:00 -0300

    Update extractTime.js

--- src/utils/extractTime.js
+++ src/utils/extractTime.js
@@ -1,8 +1,13 @@
 export function extractTime(dateString) {
   const date = new Date(dateString);
+
+  const year = date.getFullYear();
+  const month = padZero(date.getMonth() + 1); // Los meses en JavaScript son 0-indexados
+  const day = padZero(date.getDate());
+
   const hours = padZero(date.getHours());
   const minutes = padZero(date.getMinutes());
-  return `${hours}:${minutes}`;
+  return `${day}/${month}/${year} ${hours}:${minutes}`;
 }
 
 // Helper function to pad single-digit numbers with a leading zero
