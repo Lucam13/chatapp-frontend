e6d36f6a4365e987baf91be977673afa08477548
DNereaNicolau <dalmanicolau@gmail.com>
2024-09-16 14:53:16 -0300

    notification in new message from other area

--- src/hooks/useListenMessages.js
+++ src/hooks/useListenMessages.js
@@ -1,24 +1,29 @@
 import { useEffect } from "react";
-
+import toast from "react-hot-toast";
 import { useSocketContext } from "../context/SocketContext";
 import useConversation from "../zustand/useConversation";
-
 import notificationSound from "../assets/sounds/notification.mp3";
 import { useDispatch } from "react-redux";
 import { newMessage } from "../redux/actions/actions";
+import useGetAreas from "./useGetAreas";
 
 const useListenMessages = () => {
   const { socket } = useSocketContext();
   const { selectedConversation } = useConversation();
-
   const dispatch = useDispatch();
+  const { areas } = useGetAreas();
 
   useEffect(() => {
     socket?.on("newMessageFromArea", (message) => {
+      // Buscar el nombre del área correspondiente al areaId
+      const area = areas.find((a) => a._id === message.areaId);
+      const areaName = area ? area.name : "desconocida"; // Manejar caso cuando no se encuentra el área
+
       if (selectedConversation?._id === message.areaId) {
         message.shouldShake = true;
         const sound = new Audio(notificationSound);
         sound.play();
+
         dispatch(
           newMessage({
             conversationId: message.areaId,
@@ -26,6 +31,20 @@ const useListenMessages = () => {
           })
         );
       } else {
+        toast(`Tienes mensajes nuevos de la área ${areaName}`, {
+          icon: '✉️',
+          duration: 12000,
+          style: {
+            background: '#ea580c', 
+            color: '#ffffff',      
+            fontSize: '18px', 
+            fontWeight: 'bold', 
+            padding: '16px',  
+            borderRadius: '10px',
+            border: '8px solid #65a30d',
+          },
+        });
+
         dispatch(
           newMessage({
             conversationId: message.areaId,
@@ -37,6 +56,7 @@ const useListenMessages = () => {
     return () => {
       socket?.off("newMessageFromArea");
     };
-  }, [socket, selectedConversation, dispatch]);
+  }, [socket, selectedConversation, dispatch, areas]); // Asegurarse de incluir 'areas' en las dependencias del useEffect
 };
+
 export default useListenMessages;
